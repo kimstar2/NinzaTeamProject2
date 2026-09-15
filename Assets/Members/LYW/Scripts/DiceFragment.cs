c4361313d59becf5f7c720f53e6e2aa161ec5dc8
leeyoonwoo <test628test1234@gmail.com>
2026-09-15 23:36:31 +0900

    주사위 면 합성 UI, 로직 대부분 작성 완료, 휴식/재련 선택 이벤트 완성, 사건 노드 UI 개편 진행

---
+++ Assets/Members/LYW/Scripts/DiceFragment.cs
@@ -0,0 +1,55 @@
+﻿using Members.LYW.Scripts.MySystem.Events;
+using Members.LYW.Scripts.System;
+using Members.LYW.Scripts.System.Events;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Members.LYW.Scripts
+{
+    [RequireComponent(typeof(Image))]
+    public class DiceFragment : MonoBehaviour, IPointerClickHandler
+    {
+        private Image _fragmentImage;
+        public DiceFragmentSO _fragment { get; private set; }
+        private bool isSelected = false;
+        public int index {get; private set;}
+        static int SeletedValue = 0;
+        
+        public void Init(DiceFragmentSO fragment)
+        {
+            _fragmentImage = GetComponent<Image>();
+            _fragment = fragment;
+        }
+
+        public void SetIndex(int num)
+        {
+            index = num;
+        }
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!isSelected)
+            {
+                if (SeletedValue >= 3) return;
+                isSelected = true;
+                SeletedValue++;
+                _fragmentImage.color = new Color(0.7f, 0.7f, 0.7f, 1);
+                EventBus.Publish(new RegisterFragmentEvent()
+                {
+                    fragment = this,
+                });
+            }
+            else
+            {
+                isSelected = false;
+                SeletedValue--;
+                _fragmentImage.color = Color.white;
+                EventBus.Publish(new UnRegisterFragmentEvent()
+                {
+                    fragment = this,
+                });
+            }
+        }
+    }
+}
