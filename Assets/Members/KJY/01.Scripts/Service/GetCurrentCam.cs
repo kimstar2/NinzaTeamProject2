75e31c215bd232aece01d05f13a624713fad8a90
kjy08136 <kimj08136@gmail.com>
2026-09-17 20:22:26 +0900

    미침 그냥

---
+++ Assets/Members/KJY/01.Scripts/Service/GetCurrentCam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DevLib.ServiceLocator;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Members.KJY._01.Scripts.Service
+{
+    public class GetCurrentCam : MonoBehaviour , IGetCurrentCam
+    {
+        public int ID { get; private set; }
+        public CinemachineCamera CineCam {get; private set;}
+
+        private void Awake()
+        {
+            ID = transform.GetInstanceID();
+            CineCam = GetComponent<CinemachineCamera>();
+            ServiceLocator.Register<IGetCurrentCam>(this);
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.UnRegister<IGetCurrentCam>(this);
+        }
+
+        public void SetValue(float v) => CineCam.Lens.OrthographicSize = v;
+    }
+}
---
+++ Assets/Members/KJY/01.Scripts/Service/IGetCurrentCam.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Cinemachine;
+
+namespace Members.KJY._01.Scripts.Service
+{
+    public interface IGetCurrentCam
+    {
+        int ID {get; }
+        public CinemachineCamera CineCam {get; }
+        public void SetValue(float v);
+    }
+}
