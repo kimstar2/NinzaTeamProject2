c10d84fbc3299cf09d8585bdbab37986d70d34f9
Lumen <eden2010.park@gmail.com>
2026-09-20 03:04:15 +0900

    검사 스킬 연출

--- Assets/Members/PSW/Code/Unit_Logic/Runtime/Skill/Logics/Combat Instinct/CombatInstinct.cs
+++ Assets/Members/PSW/Code/Unit_Logic/Runtime/Skill/Logics/Combat Instinct/CombatInstinct.cs
@@ -4,6 +4,7 @@ using DG.Tweening;
 using Members.KJY._01.Scripts.Agent;
 using Members.KJY._01.Scripts.Agent.SkillSystem;
 using Members.KJY._01.Scripts.Agent.SkillSystem.Skill;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -37,6 +38,7 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
         public UnityEvent onLineEnd;
 
         public UnityEvent onEffectStart;
+        public UnityEvent onEffectImpact;
         public UnityEvent onEffectEnd;
     }
     
@@ -45,6 +47,9 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
         [Header("테스트용")] 
         [SerializeField] private GameObject attacker;
         [SerializeField] private GameObject target;
+
+        [Header("카메라")] 
+        [SerializeField] private CinemachineCamera targetCam;
         
         [Header("값 세팅")] 
         [SerializeField] private DurationSetting timeSet;
@@ -56,8 +61,9 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
         [SerializeField] private LineRenderer subLine1;
         [SerializeField] private LineRenderer subLine2;
 
-        [Header("이펙트 관련")]
+        [Header("이펙트 관련")] 
         [SerializeField] private ParticleSystem effect;
+        [SerializeField] private ParticleSystem shinyEffect;
         [SerializeField] private EffectEvent effectEvent;
 
         [Header("연출 관련")] 
@@ -79,12 +85,14 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
 
             _startPos = executor.Attacker.DefaultPosition.position;
             
-            mainLine.SetPosition(0, _startPos);
-            mainLine.SetPosition(1, _startPos);
-            subLine1.SetPosition(0, _startPos + Vector3.right * lineDistant.sub1Start + Vector3.up * lineDistant.upSubLine);
-            subLine1.SetPosition(1, _startPos + Vector3.right * lineDistant.sub1Start + Vector3.up * lineDistant.upSubLine);
-            subLine2.SetPosition(0, _startPos + Vector3.right * lineDistant.sub2Start + Vector3.up * lineDistant.downSubLine);
-            subLine2.SetPosition(1, _startPos + Vector3.right * lineDistant.sub2Start + Vector3.up * lineDistant.downSubLine);
+            vignette.alpha = 0;
+            
+            mainLine.SetPosition(0, Vector3.zero);
+            mainLine.SetPosition(1, Vector3.zero);
+            subLine1.SetPosition(0, Vector3.right * lineDistant.sub1Start + Vector3.up * lineDistant.upSubLine);
+            subLine1.SetPosition(1, Vector3.right * lineDistant.sub1Start + Vector3.up * lineDistant.upSubLine);
+            subLine2.SetPosition(0, Vector3.right * lineDistant.sub2Start + Vector3.up * lineDistant.downSubLine);
+            subLine2.SetPosition(1, Vector3.right * lineDistant.sub2Start + Vector3.up * lineDistant.downSubLine);
             
             Vector2 dir = executor.Target.DefaultPosition.position - executor.Attacker.DefaultPosition.position;
             dir *= endLength;
@@ -95,6 +103,7 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
         {
             if (_showLine)
             {
+                // Vector3 dir = _startPos - Executor.Attacker.DefaultPosition.position;
                 Vector3 dir = _startPos - attacker.transform.position;
                 
                 mainLine.SetPosition(0, dir); //나중에 Executor.Attacker로 변경
@@ -133,16 +142,35 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
             seq.Append(attacker.transform.DOMove(_endPos, timeSet.attackerMoveDuration));
             seq.AppendInterval(timeSet.waitFadeLineTime);
             
+            seq.AppendCallback(() =>
+            {
+                targetCam.Target.TrackingTarget = target.transform;
+                targetCam.Priority = 15;
+            });
             LineFadeOut(seq);
 
             seq.AppendCallback(() => effect.transform.position = target.transform.position);
+            seq.AppendCallback(() => shinyEffect.transform.position = target.transform.position);
             seq.Append(vignette.DOFade(1, 1f));
             seq.AppendCallback(() => effectEvent.onEffectStart?.Invoke());
-            seq.AppendInterval(timeSet.effectDuration);
-            seq.AppendCallback(() => effectEvent.onEffectEnd?.Invoke());
-            seq.Append(vignette.DOFade(0, 1f));
             
+            seq.AppendInterval(timeSet.effectDuration/2);
+            seq.AppendCallback(() =>
+            {
+                effectEvent.onEffectImpact?.Invoke();
+                targetCam.Lens.OrthographicSize = 5;
+            });
             seq.AppendCallback(() => ApplyStat());
+            seq.AppendInterval(timeSet.effectDuration/2);
+            
+            seq.AppendCallback(() => effectEvent.onEffectEnd?.Invoke());
+            seq.AppendCallback(() =>
+            {
+                targetCam.Priority = 0;
+            });
+            seq.Append(vignette.DOFade(0, 1f));
+            seq.Append(attacker.transform.DOMove(_startPos, 1f));
+            seq.AppendCallback(() => onSkillFinished?.Invoke());
         }
         
         public override void Execute()
@@ -150,9 +178,40 @@ namespace Members.PSW.Code.Unit_Logic.Runtime.Skill.Logics.Combat_Instinct
             _showLine = true;
             Sequence seq = DOTween.Sequence();
 
+            seq.AppendInterval(timeSet.attackWaitDuration);
+            seq.AppendCallback(() => effectEvent.onLineStart?.Invoke());
             seq.Append(Executor.Attacker.DefaultPosition.DOMove(_endPos, timeSet.attackerMoveDuration));
             seq.AppendInterval(timeSet.waitFadeLineTime);
+            
+            seq.AppendCallback(() =>
+            {
+                targetCam.Target.TrackingTarget = Executor.Target.DefaultPosition;
+                targetCam.Priority = 15;
+            });
             LineFadeOut(seq);
+
+            seq.AppendCallback(() => effect.transform.position = Executor.Target.DefaultPosition.position);
+            seq.AppendCallback(() => shinyEffect.transform.position = Executor.Target.DefaultPosition.position);
+            seq.Append(vignette.DOFade(1, 1f));
+            seq.AppendCallback(() => effectEvent.onEffectStart?.Invoke());
+            
+            seq.AppendInterval(timeSet.effectDuration/2);
+            seq.AppendCallback(() =>
+            {
+                effectEvent.onEffectImpact?.Invoke();
+                targetCam.Lens.OrthographicSize = 5;
+            });
+            seq.AppendCallback(() => ApplyStat());
+            seq.AppendInterval(timeSet.effectDuration/2);
+            
+            seq.AppendCallback(() => effectEvent.onEffectEnd?.Invoke());
+            seq.AppendCallback(() =>
+            {
+                targetCam.Priority = 0;
+            });
+            seq.Append(vignette.DOFade(0, 1f));
+            seq.Append(Executor.Attacker.DefaultPosition.DOMove(_startPos, 1f));
+            seq.AppendCallback(() => onSkillFinished?.Invoke());
         }
 
         private void LineFadeOut(Sequence seq)
