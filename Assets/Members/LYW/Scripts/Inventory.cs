ea4457782c7983e7ce6e079a925a5d692599e8a5
Lumen <eden2010.park@gmail.com>
2026-09-26 02:00:14 +0900

    인벤토리 프로토타입

--- Assets/Members/LYW/Scripts/Inventory.cs
+++ Assets/Members/LYW/Scripts/Inventory.cs
@@ -1,12 +1,26 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField, Min(1)] private int maxSlots = 50;
+    public int MaxSlots => Mathf.Max(1, maxSlots);
     [field : SerializeField] public List<DiceFragmentSO> DiceFragments { get; private set; } = new();
+
+    public event Action Changed;
+
+    public bool AddFragment(DiceFragmentSO fragment)
+    {
+        if (fragment == null || DiceFragments.Count >= MaxSlots) return false;
+        DiceFragments.Add(fragment);
+        Changed?.Invoke();
+        return true;
+    }
 
     public void RemoveFragment(DiceFragmentSO fragment)
     {
-        DiceFragments.Remove(fragment);
+        if (DiceFragments.Remove(fragment))
+            Changed?.Invoke();
     }
 }
